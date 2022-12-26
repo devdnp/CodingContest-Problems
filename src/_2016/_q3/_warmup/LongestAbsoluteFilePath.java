1d82a0be71f049cef76a5fc70e93159ea2fc5bc6
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-26 22:29:14 +0530

    Problem #388

---
+++ src/_2016/_q3/_warmup/LongestAbsoluteFilePath.java
@@ -0,0 +1,20 @@
+package _2016._q3._warmup;
+//#388
+public class LongestAbsoluteFilePath {
+    public static void main(String[] args) {
+        String input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+        //"dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext";
+        System.out.println(lengthLongestPath(input));
+    }
+    static int lengthLongestPath(String input) {
+        String[] paths = input.split("\n");
+        int[] stack = new int[paths.length+1];
+
+        int maxLen=0;
+        for(String s : paths){
+            int lev = s.lastIndexOf("\t")+1, curLen = stack[lev+1] = stack[lev]+s.length()-lev+1;
+            if(s.contains(".")) maxLen = Math.max(maxLen, curLen-1);
+        }
+        return maxLen;
+    }
+}
