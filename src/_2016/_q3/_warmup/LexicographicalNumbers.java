1755b8d761cdca3c7059f868fbbd51d5ad351b97
durgeshvipul <durgeshpandey419@gmail.com>
2022-12-26 22:28:36 +0530

    Problem #386

---
+++ src/_2016/_q3/_warmup/LexicographicalNumbers.java
@@ -0,0 +1,33 @@
+package _2016._q3._warmup;
+//#386
+import java.util.ArrayList;
+import java.util.List;
+
+//21-08-2016
+public class LexicographicalNumbers {
+    public static void main(String[] args) {
+        int n = 13;
+        System.out.println(lexicalOrder(n));
+    }
+
+    static List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<Integer>();
+        int curr = 1;
+
+        for(int i=1; i<=n; i++){
+            result.add(curr);
+
+            if(curr*10<=n){
+                curr*=10;
+            }else  if(curr+1<=n && curr%10!=9){
+                curr+=1;
+            }else{
+                while(curr/10 %10 == 9){
+                    curr/=10;
+                }
+                curr  = curr/10 +1;
+            }
+        }
+        return result;
+    }
+}
