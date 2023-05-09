420f43c3316a7b0e418ee36021626fc5aae0bc42
durgeshvipul <durgeshpandey419@gmail.com>
2023-05-09 23:49:40 +0530

    BS P-23 #153 (Recheck Data)

--- src/_leetcode/binary_search/_12/MinInRotatedSortedArray.java
+++ src/_leetcode/binary_search/_12/MinInRotatedSortedArray.java
@@ -4,7 +4,8 @@ package _leetcode.binary_search._12;
 public class MinInRotatedSortedArray {
 
     public static void main(String[] args) {
-        int[] nums = {3,4,5,1,2};
+        //int[] nums = {3,4,5,1,2};
+        int[] nums = {4,5,6,7,0,1,2};
         System.out.println(findMin(nums));
     }
     public static int findMin(int[] nums) {
