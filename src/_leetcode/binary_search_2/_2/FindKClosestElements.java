7b4ce643a106d2f0b1cc9621c3cde29804ca6260
durgeshvipul <durgeshpandey419@gmail.com>
2023-05-11 23:36:25 +0530

    BS-2 P-03 #658

---
+++ src/_leetcode/binary_search_2/_2/FindKClosestElements.java
@@ -0,0 +1,25 @@
+package _leetcode.binary_search_2._2;
+//Binary-Search-2
+//#658
+import java.util.*;
+
+public class FindKClosestElements {
+    public static void main(String[] args) {
+        int[]  input = {1,2,3,4,5};
+        int k = 4, x = 3;
+        System.out.println(findClosestElements(input,k,x));
+    }
+
+    public static List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<int[]> container = new PriorityQueue<>((int[] a, int[] b) -> (a[0] == b[0]) ? -(a[1]-b[1]) : -(a[0]-b[0]));
+        for (int num : arr) {
+            container.add(new int[] { Math.abs(x - num), num });
+            if (container.size() > k) container.poll();
+        }
+
+        List<Integer> nums = new ArrayList<>();
+        while (!container.isEmpty()) nums.add(container.poll()[1]);
+        Collections.sort(nums);
+        return nums;
+    }
+}
